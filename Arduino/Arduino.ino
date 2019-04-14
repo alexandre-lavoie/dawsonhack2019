c0e5b7d39e86969b6039ff6c86766a8851b24eae
Alexandre Lavoie <tatti6204@hotmail.com>
2019-04-14 13:51:20 -0400

    Cleaned up Arduino code and README.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,23 +1,34 @@
 /*
- * Code created by Alexandre Lavoie of team int kiwi at Dawson Hackathon 2019.
+ * Code created by Alexandre Lavoie of team int kiwi during Dawson Hackathon 2019.
  */
+
+int kiwi = 255; // N.P - <3
 
 // Includes
 #include <Servo.h>
 #include <IRremote.h>
 
-// Pins
+// Color Sensors -> S0-S3 are pins on the color sensor. I realized during the competition I could put them in parallel.
 #define S0 2
 #define S1 3
 #define S2 4
 #define S3 5
-#define LEFT_OUT 7
-#define RIGHT_OUT 6
+#define LEFT_OUT 7 // Left Color Sensor Out
+#define RIGHT_OUT 6 // Right Color Sensor Out
+
+// Servos
 #define LEFT_MOTOR_PIN 9
 #define RIGHT_MOTOR_PIN 10
-#define IR_PIN 11
+Servo left_motor;
+Servo right_motor;
+// Servo Calibration
+#define LEFT_MOTOR_ZERO 90
+#define RIGHT_MOTOR_ZERO 90
+#define LEFT_MOTOR_SPEED 28
+#define RIGHT_MOTOR_SPEED -20
+#define TURN_DELAY 250
 
-// LED SEGMENT
+// 7 Segment Display
 #define L1 A0
 #define L2 A1
 #define L3 A2
@@ -25,9 +36,12 @@
 #define L5 A4
 #define L6 A5
 #define L7 12
-int light = 0;
+// 7 Segment Display Timing and States for Drawings
+int state = -1;
+int timer = 0;
 
-// IR Controller
+// IR Receiver
+#define IR_PIN 11
 #define BUTTON_OK 0xFF38C7
 #define BUTTON_LEFT 0xFF10EF
 #define BUTTON_RIGHT 0xFF5AA5
@@ -38,30 +52,13 @@ int light = 0;
 #define BUTTON_1 0xFFA25D
 #define BUTTON_2 0xFF02FD
 #define BUTTON_3 0xFFE21D
-#define HOLD 0xFFFFFF
+#define HOLD 0xFFFFFF // Not used, but I realized the IR Controller sent this value when any button held (Better controls for turning?)
 IRrecv irrecv(IR_PIN);
 decode_results results;
 
-// Servo Calibration Constants
-#define LEFT_MOTOR_ZERO 90
-#define RIGHT_MOTOR_ZERO 90
-#define LEFT_MOTOR_SPEED 28
-#define RIGHT_MOTOR_SPEED -20
-#define TURN_DELAY 250
-
-// Servos
-Servo left_motor;
-Servo right_motor;
-
 // States
-bool auto_pilot = false;
-bool moving_left = false;
-bool moving_right = false;
-bool drive_forward = false;
-
-// Light
-int state = -1;
-int timer = 0;
+bool auto_pilot = false; // Swtich between line following and manual control.
+bool drive_forward = false; // On-Off Drive Forward (To prevent drive forward coming off while turning)
 
 void setup() {
   // Color Sensor
@@ -71,6 +68,7 @@ void setup() {
   pinMode(S3, OUTPUT);
   pinMode(LEFT_OUT, INPUT);
   pinMode(RIGHT_OUT, INPUT);
+  // S0-S3 Set in Setup because only looking at Clear Filter at 20%
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
   digitalWrite(S2, HIGH);
@@ -86,7 +84,7 @@ void setup() {
   pinMode(L7, OUTPUT);
   digitalWrite(L1, HIGH);
   
-  // Remote Sensor
+  // IR Sensor
   irrecv.enableIRIn();
 
   // Servos
@@ -98,7 +96,7 @@ void setup() {
 }
 
 /*
- * Draws LED.
+ * Turns on/off LEDs on the 7 Segment Display.
  */
 
 void led_display_draw(bool l1=false, bool l2=false, bool l3=false, bool l4=false, bool l5=false, bool l6=false, bool l7=false){
@@ -112,68 +110,51 @@ void led_display_draw(bool l1=false, bool l2=false, bool l3=false, bool l4=false
 }
 
 /*
- * Function to draw LED.
+ * Updates the 7 Segment Display with hardcoded LED drawings.
  */
 
 void led_display_update(){
   if(timer >= 10000) {state ++; timer=0;}
   if(state>8) state = 0;
 
-  // 1 : LEFT-TOP, 2: RIGHT-BOTTOM, 3: TOP-TOP, 4: LEFT-BOTTOM, 5: BOTTOM-BOTTOM, 6: TOP-RIGHT, 7: MIDDLE-MIDDLE
+  // For Drawing Purpose - 1 : LEFT-TOP, 2: RIGHT-BOTTOM, 3: TOP-TOP, 4: LEFT-BOTTOM, 5: BOTTOM-BOTTOM, 6: TOP-RIGHT, 7: MIDDLE-MIDDLE
 
   switch(state){
     case 0:
-      led_display_draw(true, true, true, true, false, true, true);
+      led_display_draw(true, true, true, true, false, true, true); // A
       break;
     case 1:
-      led_display_draw(true, true, false, true, false, false, false);
+      led_display_draw(true, true, false, true, false, false, false); // L
       break;
     case 2:
-      led_display_draw(true, false, true, true, true, false, true);
+      led_display_draw(true, false, true, true, true, false, true); // E
       break;
     case 3:
-      led_display_draw(true, true, false, true, false, true, true);
+      led_display_draw(true, true, false, true, false, true, true); // X
       break;
     case 4:
-      led_display_draw();
+      led_display_draw(); // EMPTY
       break;
     case 5:
-      led_display_draw(true, true, false, true, true, true, false);
+      led_display_draw(true, true, false, true, true, true, false); // Smiley -> :)
       break;
     case 6:
-      led_display_draw();
+      led_display_draw(); // Empty
       break;
     case 7:
-      led_display_draw(true, true, false, true, true, true, false);
+      led_display_draw(true, true, false, true, true, true, false); // Smiley -> :)
       break;
     case 8:
-      led_display_draw();
+      led_display_draw(); // Empty
       break;
     default:
-      led_display_draw(true, true, true, true, true, true, true);
+      led_display_draw(true, true, true, true, true, true, true); // If there is a state error, display full segment.
       break;
-  }
-}
-
-void loop() {
-  
-  scan_controller();
-
-  led_display_update();
-  
-  if(auto_pilot == true){
-    timer+=1000;
-    line_follow();
-  }else{
-    timer++;
-    if(drive_forward){
-      drive(LEFT_MOTOR_SPEED, RIGHT_MOTOR_SPEED);
-    }
   }
 }
 
 /*
- * Gets controller input and applies function.
+ * Get controller inputs and performs functions using switch statment.
  */
 
 void scan_controller() {
@@ -224,7 +205,7 @@ void scan_controller() {
 }
 
 /*
- * Uses the servos to drive the robot, given the shift.
+ * Continuous Driving for Servos unless if Overwritten -> Forward/Backward Driving
  */
 
 void drive(float left_motor_speed, float right_motor_speed){
@@ -232,30 +213,30 @@ void drive(float left_motor_speed, float right_motor_speed){
   right_motor.write(RIGHT_MOTOR_ZERO + right_motor_speed);
 }
 
+/*
+ * Timed Driving for Servos -> Turning
+ */
+
 void drive_and_stop(float left_motor_speed, float right_motor_speed, int del){
   drive(left_motor_speed, right_motor_speed);
   delay(del);
   drive(0,0);  
 }
 
-void drive_and_go(float left_motor_speed, float right_motor_speed, int del){
-  drive(left_motor_speed, right_motor_speed);
-  delay(del);
-  drive(LEFT_MOTOR_SPEED, RIGHT_MOTOR_SPEED);  
-}
-
 /*
- * Line following.
+ * Line Following Algorithm.
  */
 
 void line_follow(){
   int frequency = pulseIn(LEFT_OUT, LOW);
   int frequency2 = pulseIn(RIGHT_OUT, LOW);
+  
   Serial.print("Frequency 1: ");
   Serial.println(frequency);
   Serial.print("Frequency 2 ");
   Serial.println(frequency2);
- 
+
+  // Checks if one color sensor sees brighter/darker values. If one sees a dark values, means they're closer to the line, thus turn the other side. (Maybe could look at the variation of both?)
   if(frequency > frequency2){
     drive(LEFT_MOTOR_SPEED / 2, RIGHT_MOTOR_SPEED);
   }else if(frequency < frequency2){
@@ -265,4 +246,21 @@ void line_follow(){
   }
 
   delay(10);
+}
+
+void loop() {
+  
+  scan_controller();
+
+  led_display_update();
+  
+  if(auto_pilot == true){
+    timer+=1000; // Timer for display (considering the 10 second delay in the line_folow function).
+    line_follow();
+  }else{
+    timer++; // Timer for display with no function running.
+    if(drive_forward){
+      drive(LEFT_MOTOR_SPEED, RIGHT_MOTOR_SPEED);
+    }
+  }
 }
--- README.md
+++ README.md
@@ -3,9 +3,35 @@ Repository for team int kiwi.
 
 # Team
 
-Alexandre Lavoie
+Alexandre Lavoie, Rayaan Kazim, Tony Wen, Daud Khan
+
+# Winner
+
+Team int kiwi won the Creativity and Ingenuity Prize.
+
+# Challenge
+
+The second Dawson College Hackathon challenge was to navigate a robot using provided equipment through a 4x8 grid that included two line following sections and one remote control maze. Winners were determined according to the following:
+
+1. Fastest and fewest penalties over the entire path.
+2. Fastest and fewest penalties in the maze.
+3. Creativity and Ingenuity of the robot desing.
+
+# Repository
+
+This repository contains all the code created prior/during the competition.
+
+## Arduino
+
+Contains the code for the bot. This includes a line follower using a color sensor, remote control, and an LED 7 segment display.
+
+## Processing
+
+Contains a code for communicating to an Arduino using Processing. This is obsolete, given the competition didn't use the Ultrasonic sensor.
 
 # Plan
+
+This was the plan of action for the competition. Using the SCRUM workflow, this competition had three main sprints: Servos, IR Receiver, and Color Sensors. A last sprint was made for design (7 segment display) and a better user experience. Following is a checklist used during the competition:
 
 ## Basic Hardware
 
@@ -17,23 +43,23 @@ Alexandre Lavoie
 
 ## Hardware Testing
 
-- [ ] Calibrate Servos
+- [x] Calibrate Servos
 - [x] Receive Basic IR Signals
-- [ ] Calibrate Color Sensors
+- [x] Calibrate Color Sensors
 
 ## Basic Software
 
 - [x] Forward / Backward + Rotation of Servos
 - [x] Executing Commands from IR Receiver
-- [ ] Receiving Color Input from Color Sensors
+- [x] Receiving Color Input from Color Sensors
 
 ## Algorithms
 
-- [ ] Line Follower
-- [ ] Line Avoider for Manual Control
+- [x] Line Follower
+- [x] Line Avoider for Manual Control
 
 ## Design Cleanups
 
-- [ ] Car Design
-- [ ] Improve IR Range
-- [ ] Improve IR Controls (Line Avoider?)
+- [x] Car Design
+- [x] Improve IR Range
+- [x] Improve IR Controls (Line Avoider?)
